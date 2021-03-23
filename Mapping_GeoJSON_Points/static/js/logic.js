01ec57ee2d4c2707c2a96ea24e7a4643984cd5b6
Paromita De <pde492@gmail.com>
2021-03-23 13:14:58 -0400

    13.5.5

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -2,7 +2,7 @@
 console.log("working");
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
@@ -23,7 +23,7 @@ let baseMaps = {
 
 // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
-    center: [44.0, -80.0],
+    center: [30, 30],
     zoom: 2,
     layers: [streets]
 });
@@ -31,27 +31,16 @@ let map = L.map('mapid', {
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(map);
 
+// Then we add our 'graymap' tile layer to the map.
+//streets.addTo(map);
 
 // Accessing the airport GeoJSON URL
-// Accessing the Toronto airline routes GeoJSON URL.
-let torontoData = "https://raw.githubusercontent.com/pde294/Mapping_Earthquakes/Mapping_GeoJSON_Linestrings/torontoRoutes.json";
-// Create a style for the lines.
-let myStyle = {
-  color: "#ffffa1",
-  weight: 2
-}
+let airportData = "https://raw.githubusercontent.com/pde294/Mapping_Earthquakes/Mapping_Multiple_Points/majorAirports.json";
+
 // Grabbing our GeoJSON data.
-d3.json(torontoData).then(function(data) {
+d3.json(airportData).then(function(data) {
     console.log(data);
   // Creating a GeoJSON layer with the retrieved data.
-  L.geoJson(data, {
-    color:"#ffffal",
-    weight:2,
-    onEachFeature: function(feature, layer) {
-      layer.blindPopup("<h3> Airline: " + feature.properties.airline + "</h3> <hr><h3> Destination: "
-        + feature.properties.dst + "</h3>"); 
-    }
-  })
-  .addTo(map);
+  L.geoJson(data).addTo(map);
 });
 
